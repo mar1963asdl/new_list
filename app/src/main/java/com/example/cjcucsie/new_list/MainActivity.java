84133a25cac4f64fbc7195f45bc8f43178ee5921
kevin870828 <kevin870828@users.noreply.github.com>
2018-07-13 15:59:51 +0800

    new_list

--- app/src/main/java/com/example/cjcucsie/new_list/MainActivity.java
+++ app/src/main/java/com/example/cjcucsie/new_list/MainActivity.java
@@ -4,83 +4,81 @@ package com.example.cjcucsie.new_list;
         import android.os.Bundle;
         import android.support.v7.widget.LinearLayoutManager;
         import android.support.v7.widget.RecyclerView;
-        import android.view.LayoutInflater;
         import android.view.View;
-        import android.view.ViewGroup;
-        import android.widget.TextView;
+        import android.widget.Button;
+        import android.widget.EditText;
         import android.widget.Toast;
+//        import android.view.LayoutInflater;
+ //       import android.view.View;
+  //      import android.view.ViewGroup;
+   //     import android.widget.EditText;
+    //    import android.widget.TextView;
+        //    import android.widget.Toast;
 
         import java.util.ArrayList;
-        import java.util.List;
+  //      import java.util.Date;
+  //      import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private MyAdapter mAdapter;
     private RecyclerView mRecyclerView;
+    private EditText edit;
+    Button txtButton;
+
+//    @Override
+//    public <T extends View> T findViewById(int id) {
+//        return super.findViewById(R.id.button);
+//    }
+
+            ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayList<String> myDataset = new ArrayList<>();
-        for(int i = 0; i < 100; i++){
-            myDataset.add(Integer.toString(i));
-        }
+        final ArrayList<String> myDataset = new ArrayList<>();
+
         mAdapter = new MyAdapter(myDataset);
+
+
+//        myDataset.add("");
+//        mAdapter.notifyDataSetChanged();
+
+        txtButton = findViewById(R.id.button);
+        txtButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                if(!edit.getText().toString().equals("")) {
+                    myDataset.add(edit.getText().toString());
+                    mAdapter.notifyDataSetChanged();
+                    edit.setText("");
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Enter item", Toast.LENGTH_SHORT).show();
+                    
+                }
+            }
+
+//            @Override
+//            public void onClick(View view) {
+//
+//                myDataset.remove();
+//                mAdapter.notifyDataSetChanged();
+//            }
+        });
+        edit = findViewById(R.id.edit1);
         mRecyclerView = (RecyclerView) findViewById(R.id.list_view);
         final LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         mRecyclerView.setLayoutManager(layoutManager);
         mRecyclerView.setAdapter(mAdapter);
-    }
-
-    public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-        private List<String> mData;
-
-        public class ViewHolder extends RecyclerView.ViewHolder {
-            public TextView mTextView;
-//            public View itemView;
-
-            public ViewHolder(View v) {
-                super(v);
-//                itemView=v;
-                mTextView = (TextView) v.findViewById(R.id.info_text);
-            }
-        }
-
-        public MyAdapter(List<String> data) {
-            mData = data;
-        }
-
-        @Override
-        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View v = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.item, parent, false);
-            ViewHolder vh = new ViewHolder(v);
-            return vh;
-        }
-
-        @Override
-        public void onBindViewHolder(ViewHolder holder, final int position) {
-            holder.mTextView.setText(mData.get(position));
-
-            holder.itemView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast.makeText(MainActivity.this, "Item " + position + " is clicked.", Toast.LENGTH_SHORT).show();
-                }
-            });
-            holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View v) {
-                    Toast.makeText(MainActivity.this, "Item " + position + " is long clicked.", Toast.LENGTH_SHORT).show();
-                    return true;
-                }
-            });
-            }
-
-
-        @Override
-        public int getItemCount() {
-            return mData.size();
-        }
+//        @Override
+//        public void onClick (View) {
+//                myDataset.add(Integer.toString(i));
+//        mAdapter.notifyDataSetChanged();
+//        }
     }
 }
+
---
+++ app/src/main/java/com/example/cjcucsie/new_list/MyAdapter.java
@@ -0,0 +1,83 @@
+package com.example.cjcucsie.new_list;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+//   import android.widget.Toast;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private List<String> mData;
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTextView;
+//            public View itemView;
+
+        public ViewHolder(View v) {
+            super(v);
+//                itemView=v;
+            txtButton1 = v.findViewById(R.id.button3);
+            mTextView = (TextView) v.findViewById(R.id.info_text);
+        }
+    }
+
+    public MyAdapter(List<String> data) {
+        mData = data;
+    }
+
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+    Button txtButton1;
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        holder.mTextView.setText(mData.get(position));
+
+
+        txtButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mData.remove(position);
+                MyAdapter.this.notifyDataSetChanged();
+            }
+        });
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               // Toast.makeText(MainActivity.this, "Item " + position + " is clicked.", Toast.LENGTH_SHORT).show();
+            }
+        });
+        holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+          //      Toast.makeText(MainActivity.this, "Item " + position + " is long clicked.", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+        });
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+//    @Override
+//    public void onClick(View view) {
+//        switch (view.getId()){
+//            case R.id.button:
+//                String itemEntered=item.getText().toString();
+//        }
+//    }
+}
